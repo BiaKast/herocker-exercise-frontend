a4fb43ed139618e7b4c01aa0ef0fbf36eafbc0b7
BiaKast <biahdrew94@gmail.com>
2022-09-06 00:06:50 -0300

    a

--- src/App.js
+++ src/App.js
@@ -2,40 +2,39 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-// const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'http://localhost:3000/users';
 
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   fetch(API_ENDPOINT)
-  //     .then((res) => res.json())
-  //     .then((parsedData) => { setData(parsedData); setLoading(false); });
-  // }, []);
+  useEffect(() => {
+    fetch(API_ENDPOINT)
+      .then((res) => res.json())
+      .then((parsedData) => { setData(parsedData); setLoading(false); });
+  }, []);
 
-  // if (loading) return <h1>loading</h1>;
+  if (loading) return <h1>loading</h1>;
 
   return (
-    <h1>oi</h1>
-    // <table>
-    //   <tr>
-    //     {Object.keys(data[0]).map(
-    //       (key) => (<th key={ Math.random() }>{key}</th>),
-    //     )}
-    //   </tr>
-    //   <tbody>
-    //     {data.map((rowInfo) => (
-    //       <tr key={ Math.random() }>
-    //         {
-    //           Object.values(rowInfo).map(
-    //             (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
-    //           )
-    //         }
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    // </table>
+    <table>
+      <tr>
+        {Object.keys(data[0]).map(
+          (key) => (<th key={ Math.random() }>{key}</th>),
+        )}
+      </tr>
+      <tbody>
+        {data.map((rowInfo) => (
+          <tr key={ Math.random() }>
+            {
+              Object.values(rowInfo).map(
+                (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
+              )
+            }
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
